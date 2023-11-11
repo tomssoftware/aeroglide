aa892cee3cf36165382dab129fbefd659491873e
thomasfausm <thomasfausm@gmx.de>
2023-11-11 14:50:10 +0100

    Nav graph and dummy screens added

---
+++ app/src/main/java/com/alpsfly/aeroglide/NavGraph.kt
@@ -0,0 +1,34 @@
+package com.alpsfly.aeroglide
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.alpsfly.aeroglide.ui.screen.Screen
+import com.alpsfly.aeroglide.ui.screen.Screen1
+import com.alpsfly.aeroglide.ui.screen.Screen2
+import com.alpsfly.aeroglide.ui.screen.Screen3
+
+@Composable
+fun SetupNavGraph(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Screen1.route
+    ) {
+        composable(
+            route = Screen.Screen1.route
+        ) {
+            Screen1(navController = navController)
+        }
+        composable(
+            route = Screen.Screen2.route
+        ) {
+            Screen2(navController = navController)
+        }
+        composable(
+            route = Screen.Screen3.route
+        ) {
+            Screen3(navController = navController)
+        }
+    }
+}
