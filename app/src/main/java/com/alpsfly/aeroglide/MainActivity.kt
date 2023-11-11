44c5c63d4a0f245baf25fa6183634231d5383057
thomasfausm <thomasfausm@gmx.de>
2023-11-11 14:50:10 +0100

    Nav graph and dummy screens added

--- app/src/main/java/com/alpsfly/aeroglide/MainActivity.kt
+++ app/src/main/java/com/alpsfly/aeroglide/MainActivity.kt
@@ -10,34 +10,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.alpsfly.aeroglide.ui.theme.AeroGlideTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var navController: NavHostController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AeroGlideTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
+            navController = rememberNavController()
+            SetupNavGraph(navController = navController)
+
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AeroGlideTheme {
-        Greeting("Android")
     }
 }
