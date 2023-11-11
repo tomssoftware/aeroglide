f50937f4009ef62892a6e03bc195d439c989c02f
thomasfausm <thomasfausm@gmx.de>
2023-11-11 14:50:10 +0100

    Nav graph and dummy screens added

---
+++ app/src/main/java/com/alpsfly/aeroglide/ui/screen/Screen.kt
@@ -0,0 +1,7 @@
+package com.alpsfly.aeroglide.ui.screen
+
+sealed class Screen(val route:String) {
+    data object Screen1 : Screen(route = "page_1")
+    data object Screen2 : Screen(route = "page_2")
+    data object Screen3 : Screen(route = "page_3")
+}
