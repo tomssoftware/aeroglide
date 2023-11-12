f285781884daee60a41b4a4cd6eba1ef8b9f4539
Thomas F <thomasfausm@gmx.de>
2023-11-12 08:29:50 +0100

    Theme added

--- app/src/main/java/com/alpsfly/aeroglide/MainActivity.kt
+++ app/src/main/java/com/alpsfly/aeroglide/MainActivity.kt
@@ -3,13 +3,6 @@ package com.alpsfly.aeroglide
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.alpsfly.aeroglide.ui.theme.AeroGlideTheme
@@ -19,9 +12,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            navController = rememberNavController()
-            SetupNavGraph(navController = navController)
-
+            AeroGlideTheme {
+                navController = rememberNavController()
+                SetupNavGraph(navController = navController)
+            }
         }
     }
 }
