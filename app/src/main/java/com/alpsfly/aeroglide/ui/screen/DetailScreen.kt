9c6a25056eece7b7a89abf0d85612a0da0763497
thomasfausm <thomasfausm@gmx.de>
2023-11-14 19:34:31 +0100

    AeroGlideBottomBar added

--- app/src/main/java/com/alpsfly/aeroglide/ui/screen/DetailScreen.kt
+++ app/src/main/java/com/alpsfly/aeroglide/ui/screen/DetailScreen.kt
@@ -1,37 +1,24 @@
 package com.alpsfly.aeroglide.ui.screen
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Info
-import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material.icons.filled.KeyboardArrowLeft
-import androidx.compose.material.icons.filled.KeyboardArrowRight
-import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.alpsfly.aeroglide.AeroGlideBottomBar
 import com.alpsfly.aeroglide.ui.page.Page1
 import com.alpsfly.aeroglide.ui.page.Page2
-import kotlinx.coroutines.launch
+import com.alpsfly.aeroglide.ui.util.MenuItem
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -41,13 +28,6 @@ fun DetailScreen(navController: NavController) {
     val pagerState = rememberPagerState(pageCount = {
         3
     })
-
-    data class MenuItem(
-        val id: String,
-        val title: String,
-        val contentDescription: String,
-        val icon: ImageVector
-    )
 
     val items: List<MenuItem> = listOf(
         MenuItem(
@@ -72,29 +52,12 @@ fun DetailScreen(navController: NavController) {
 
     Scaffold(
         bottomBar = {
-            BottomAppBar {
-                LazyRow(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 16.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween
-                ) {
-                    itemsIndexed(items) { page, item ->
-                        IconButton(
-                            onClick = {
-                                coroutineScope.launch {
-                                    pagerState.scrollToPage(page, 0f)
-                                }
-                            }
-                        ) {
-                            Icon(
-                                imageVector = item.icon,
-                                contentDescription = item.contentDescription
-                            )
-                        }
-                    }
-                }
-            }
+            AeroGlideBottomBar(
+                modifier = Modifier,
+                items = items,
+                pagerState = pagerState,
+                coroutineScope = coroutineScope
+            )
         }
     ) {
         Box(
