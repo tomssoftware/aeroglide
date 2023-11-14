faa414807773199068408e9a1a787dc7cd77956e
thomasfausm <thomasfausm@gmx.de>
2023-11-14 18:49:45 +0100

    BottomAppBar iterator added

--- app/src/main/java/com/alpsfly/aeroglide/ui/screen/DetailScreen.kt
+++ app/src/main/java/com/alpsfly/aeroglide/ui/screen/DetailScreen.kt
@@ -6,13 +6,19 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -20,6 +26,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.alpsfly.aeroglide.ui.page.Page1
@@ -32,65 +39,59 @@ fun DetailScreen(navController: NavController) {
 
     val coroutineScope = rememberCoroutineScope()
     val pagerState = rememberPagerState(pageCount = {
-        4
+        3
     })
+
+    data class MenuItem(
+        val id: String,
+        val title: String,
+        val contentDescription: String,
+        val icon: ImageVector
+    )
+
+    val items: List<MenuItem> = listOf(
+        MenuItem(
+            id = "home",
+            title = "Home",
+            contentDescription = "Go to home screen",
+            icon = Icons.Default.Home
+        ),
+        MenuItem(
+            id = "settings",
+            title = "Settings",
+            contentDescription = "Go to settings screen",
+            icon = Icons.Default.Settings
+        ),
+        MenuItem(
+            id = "help",
+            title = "Help",
+            contentDescription = "Get help",
+            icon = Icons.Default.Info
+        ),
+    )
 
     Scaffold(
         bottomBar = {
             BottomAppBar {
-                Row(
+                LazyRow(
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(horizontal = 16.dp),
                     horizontalArrangement = Arrangement.SpaceBetween
                 ) {
-                    IconButton(
-                        onClick = {
-                            coroutineScope.launch {
-                                pagerState.scrollToPage(0, 0f)
+                    itemsIndexed(items) { page, item ->
+                        IconButton(
+                            onClick = {
+                                coroutineScope.launch {
+                                    pagerState.scrollToPage(page, 0f)
+                                }
                             }
+                        ) {
+                            Icon(
+                                imageVector = item.icon,
+                                contentDescription = item.contentDescription
+                            )
                         }
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.KeyboardArrowUp,
-                            contentDescription = "Mark as favorite"
-                        )
-                    }
-                    IconButton(
-                        onClick = {
-                            coroutineScope.launch {
-                                pagerState.scrollToPage(1, 0f)
-                            }
-                        }
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.KeyboardArrowDown,
-                            contentDescription = "Edit notes"
-                        )
-                    }
-                    IconButton(
-                        onClick = {
-                            coroutineScope.launch {
-                                pagerState.scrollToPage(2, 0f)
-                            }
-                        }
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.KeyboardArrowLeft,
-                            contentDescription = "Mark as favorite"
-                        )
-                    }
-                    IconButton(
-                        onClick = {
-                            coroutineScope.launch {
-                                pagerState.scrollToPage(3, 0f)
-                            }
-                        }
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.KeyboardArrowRight,
-                            contentDescription = "Mark as favorite"
-                        )
                     }
                 }
             }
