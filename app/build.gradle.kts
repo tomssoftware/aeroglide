e81ed64640eae592874b68bb3e1e16297c2bbd18
thomasfausm <thomasfausm@gmx.de>
2023-11-22 19:26:55 +0100

    Sensor repo impl.
    Show accel data in screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.navigation:navigation-runtime-ktx:2.7.5")
     implementation("androidx.navigation:navigation-compose:2.7.5")
+    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
     implementation("com.google.dagger:hilt-android:2.48.1")
     kapt("com.google.dagger:hilt-android-compiler:2.48.1")
     testImplementation("junit:junit:4.13.2")
