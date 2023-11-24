244822c6d45de78590ed5d61e55b0889fce3eb4f
thomasfausm <thomasfausm@gmx.de>
2023-11-24 22:01:21 +0100

    Vico graph lib added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,15 @@ dependencies {
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
     implementation("com.google.dagger:hilt-android:2.48.1")
     kapt("com.google.dagger:hilt-android-compiler:2.48.1")
+
+    // vico charts
+    implementation("com.patrykandpatrick.vico:compose:1.13.0")
+    implementation("com.patrykandpatrick.vico:compose-m3:1.13.0")
+    implementation("com.patrykandpatrick.vico:core:1.13.0")
+
+    // timber logger
+    implementation ("com.jakewharton.timber:timber:5.0.1")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/alpsfly/aeroglide/ui/page/Page2.kt
+++ app/src/main/java/com/alpsfly/aeroglide/ui/page/Page2.kt
@@ -8,14 +8,26 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.alpsfly.aeroglide.ui.screen.Screen
+import com.alpsfly.aeroglide.viewmodel.SensorViewModel
+import com.patrykandpatrick.vico.compose.axis.horizontal.rememberBottomAxis
+import com.patrykandpatrick.vico.compose.axis.vertical.rememberStartAxis
+import com.patrykandpatrick.vico.compose.chart.Chart
+import com.patrykandpatrick.vico.compose.chart.line.lineChart
+import com.patrykandpatrick.vico.core.entry.entryModelOf
 
 @Composable
-fun Page2() {
+fun Page2(sensorViewModel: SensorViewModel = hiltViewModel()) {
+    val avgPressure by sensorViewModel.avgPressureFlow.collectAsState(initial = 0f)
+    val avgPressureList by sensorViewModel.avgPressureStateFlow.collectAsState(initial = 0f)
+    val chartEntryModel = entryModelOf(4f, 12f, 8f, 16f)
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -27,10 +39,17 @@ fun Page2() {
             verticalArrangement = Arrangement.Center
         )
         {
-            Text(text = "this is ${Screen.Screen2.route}",
+            Text(text = "this is $avgPressure, $avgPressureList",
                 modifier = Modifier.clickable {
                 }
+            )
+            Chart(
+                chart = lineChart(),
+                model = chartEntryModel,
+                startAxis = rememberStartAxis(),
+                bottomAxis = rememberBottomAxis(),
             )
         }
     }
 }
+
