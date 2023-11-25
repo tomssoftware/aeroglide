c884ed77739afc671ecd1285dfda6a4ac104e73f
thomasfausm <thomasfausm@gmx.de>
2023-11-25 17:53:09 +0100

    Timber added

--- app/src/main/java/com/alpsfly/aeroglide/AeroGlideApplication.kt
+++ app/src/main/java/com/alpsfly/aeroglide/AeroGlideApplication.kt
@@ -2,6 +2,21 @@ package com.alpsfly.aeroglide
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 @HiltAndroidApp
-class AeroGlideApplication : Application()
+class AeroGlideApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        applicationScope.launch {
+            if (BuildConfig.DEBUG) {
+                Timber.plant(Timber.DebugTree())
+            }
+        }
+    }
+}
+
+private val applicationScope = CoroutineScope(Dispatchers.Default)
