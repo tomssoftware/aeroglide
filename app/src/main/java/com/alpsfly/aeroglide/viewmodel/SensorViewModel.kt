75ff377114100cf46e7b51e90297399213768472
thomasfausm <thomasfausm@gmx.de>
2023-11-25 14:10:32 +0100

    Show dynamic data in chart

--- app/src/main/java/com/alpsfly/aeroglide/viewmodel/SensorViewModel.kt
+++ app/src/main/java/com/alpsfly/aeroglide/viewmodel/SensorViewModel.kt
@@ -3,14 +3,21 @@ package com.alpsfly.aeroglide.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.alpsfly.aeroglide.data.repository.SensorRepository
+import com.patrykandpatrick.vico.core.entry.ChartEntryModelProducer
+import com.patrykandpatrick.vico.core.entry.FloatEntry
+import com.patrykandpatrick.vico.core.entry.entryOf
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.buffer
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.shareIn
 import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
+import kotlin.random.Random
 
 @HiltViewModel
 class SensorViewModel @Inject constructor(
@@ -21,26 +28,18 @@ class SensorViewModel @Inject constructor(
         started = SharingStarted.WhileSubscribed(5000)
     )
 
-    val avgPressureFlow = sensorRepository.avgPressureFlow.shareIn(
+    private val queue = ArrayDeque<FloatEntry>()
+    val chartEntryModelProducer = ChartEntryModelProducer(queue)
+    val avgPressureFlow = sensorRepository.avgPressureFlow.map {
+        if (queue.size == 10) {
+            queue.removeFirst()
+        }
+        queue.addLast(FloatEntry(queue.size.toFloat(), it))
+        Timber.d("Size ${queue.size}")
+        chartEntryModelProducer.setEntries(queue)
+        it
+    }.shareIn(
         scope = viewModelScope,
         started = SharingStarted.WhileSubscribed(5000)
     )
-
-    val avgPressureStateFlow = MutableStateFlow(emptyList<Float>())
-    private fun getPressureList() {
-        viewModelScope.launch { Dispatchers.IO
-            sensorRepository.avgPressureFlow.collect {value ->
-                val queue = avgPressureStateFlow.value
-                if (queue.size < 10) {
-                    avgPressureStateFlow.value = queue + value
-                } else {
-                    avgPressureStateFlow.value = queue.drop(1) + value
-                }
-            }
-        }
-    }
-
-    init {
-        getPressureList()
-    }
 }
