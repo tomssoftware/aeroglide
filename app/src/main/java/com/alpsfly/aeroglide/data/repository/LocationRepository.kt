5230fcf94852674207caf9970a0e03333dfb5b35
thomasfausm <thomasfausm@gmx.de>
2023-12-02 11:49:40 +0100

    Make repos singleton

--- app/src/main/java/com/alpsfly/aeroglide/data/repository/LocationRepository.kt
+++ app/src/main/java/com/alpsfly/aeroglide/data/repository/LocationRepository.kt
@@ -7,11 +7,12 @@ import com.alpsfly.aeroglide.data.util.locationDataFlow
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
+import javax.inject.Singleton
 
 interface LocationRepository {
     val locationDataSource: Flow<Location>
 }
-
+@Singleton
 class LocationRepositoryImpl @Inject constructor(
     @ApplicationContext private val context: Context
 ) : LocationRepository {
