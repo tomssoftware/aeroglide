9f35ce756b792037efbf812dd1393b4aa775bdca
Thomas Fiedler <thomas.fiedler@fn.de>
2024-12-23 09:57:28 +0100

    Clean up code

--- app/src/main/java/com/alpsfly/aeroglide/feature/variometer/presentation/VariometerScreen.kt
+++ app/src/main/java/com/alpsfly/aeroglide/feature/variometer/presentation/VariometerScreen.kt
@@ -2,11 +2,9 @@ package com.alpsfly.aeroglide.feature.variometer.presentation
 
 import android.graphics.RectF
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -17,13 +15,11 @@ import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.DrawModifier
 import androidx.compose.ui.geometry.Offset
@@ -45,25 +41,11 @@ import androidx.navigation.NavController
 import com.alpsfly.aeroglide.AeroGlideBottomBar
 import com.alpsfly.aeroglide.core.data.util.SensorData
 import com.alpsfly.aeroglide.core.ui.MenuItem
+import com.alpsfly.aeroglide.core.ui.centered
 import com.alpsfly.aeroglide.core.ui.presentation.AltitudeProfileScreen
 import com.alpsfly.aeroglide.core.ui.presentation.ClimbrateProfileScreen
 import com.alpsfly.aeroglide.core.ui.presentation.FlightStatusScreen
-import com.alpsfly.aeroglide.core.ui.presentation.Screen1
-import com.alpsfly.aeroglide.core.ui.viewmodel.SensorViewModel
-import com.alpsfly.aeroglide.feature.devicestatus.viewmodel.VicoChartViewModel
 import com.alpsfly.aeroglide.feature.variometer.viewmodel.VarioViewModel
-
-
-class CenteredModifier : DrawModifier {
-    override fun ContentDrawScope.draw() {
-        val center = Offset(size.width / 2, size.height / 2)
-        translate(center.x, center.y) {
-            this@draw.drawContent()
-        }
-    }
-}
-
-fun Modifier.centered() = this.then(CenteredModifier())
 
 private var startScaleAngle = 180f
 private var climbrateAverage = 0f
@@ -85,7 +67,6 @@ private fun getOnScaleAngleEnd(climbrate: Float) = if (climbrate < 0f) {
 } else {
     startScaleAngle + 1f + 30f * climbrate
 }
-
 
 @Composable
 fun VariometerScreen(
@@ -127,40 +108,35 @@ fun VariometerScreen(
             )
         }
     ) {
-        Box(
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
+                .fillMaxSize()
                 .padding(it)
         ) {
-            HorizontalPager(state = pagerState) { page ->
-                when (page) {
-                    0 -> {
-                        AnalogVariometer(Modifier.fillMaxSize())
-                    }
+            FlightStatusScreen(Modifier.fillMaxWidth(), navController)
+            Spacer(Modifier.weight(2f))
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(it)
+            ) {
+                HorizontalPager(state = pagerState) { page ->
+                    when (page) {
+                        0 -> {
+                            AnalogVariometer(Modifier.fillMaxSize())
+                        }
 
-                    1 -> {
-                        AltitudeProfileScreen(Modifier.fillMaxSize(), navController)
-                    }
+                        1 -> {
+                            AltitudeProfileScreen(Modifier.fillMaxSize(), navController)
+                        }
 
-                    2 -> {
-                        ClimbrateProfileScreen(Modifier.fillMaxSize(), navController)
+                        2 -> {
+                            ClimbrateProfileScreen(Modifier.fillMaxSize(), navController)
+                        }
                     }
                 }
             }
-        }
-    }
 
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Top
-        ) {
-            FlightStatusScreen(Modifier.fillMaxWidth(), navController)
-            //AnalogVariometer(Modifier.fillMaxSize())
         }
     }
 }
