7308646432f1c8b6662108980c830fd3a3ebb201
Thomas Fiedler <thomas.fiedler@fn.de>
2024-12-24 15:49:32 +0100

    Bump gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,8 @@ buildscript {
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.25" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
+    id("com.google.devtools.ksp") version "2.0.0-1.0.24" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
 }
