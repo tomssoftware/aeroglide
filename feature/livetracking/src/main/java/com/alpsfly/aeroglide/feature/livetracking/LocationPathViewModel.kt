fd088abd7fca7d139d1766c3c850093c627fff6f
Thomas Fiedler <thomas.fiedler@fn.de>
2025-02-17 21:13:51 +0100

    Live tracking feature improved

--- feature/livetracking/src/main/java/com/alpsfly/aeroglide/feature/livetracking/LocationPathViewModel.kt
+++ feature/livetracking/src/main/java/com/alpsfly/aeroglide/feature/livetracking/LocationPathViewModel.kt
@@ -1,8 +1,11 @@
 package com.alpsfly.aeroglide.feature.livetracking
 
+import androidx.compose.ui.graphics.Color
+import androidx.core.graphics.toColorInt
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.alpsfly.aeroglide.core.data.SensorRepository
+import com.alpsfly.aeroglide.core.mapbox.data.MapBoxLocation
 import com.alpsfly.aeroglide.core.mapbox.data.mapToFeatureCollection
 import com.alpsfly.aeroglide.core.mapbox.data.zipMapBoxLocations
 import com.alpsfly.aeroglide.core.model.database.Climbrate
@@ -14,6 +17,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -22,34 +26,26 @@ class LocationPathViewModel @Inject constructor(
     private val sensorRepository: SensorRepository,
 ) : ViewModel() {
 
-    private var _mapboxFeatureCollection = FeatureCollection.fromFeatures(emptyList())
-    val mapboxFeatureCollection: FeatureCollection
-        get() {
-            return _mapboxFeatureCollection
-        }
-
-    private val _mapboxFeatureCollectionFlow = MutableStateFlow<FeatureCollection>(FeatureCollection.fromFeatures(emptyList()))
-    val mapboxFeatureCollectionFlow: StateFlow<FeatureCollection> = _mapboxFeatureCollectionFlow
-
     private val _mapboxPointCollection = MutableStateFlow<List<Point>>(emptyList())
     val mapboxPointCollection: StateFlow<List<Point>> = _mapboxPointCollection
 
-    fun loadFeatureCollection() {
-        viewModelScope.launch {
-            getMapboxFeatureCollection().collect { featureCollection ->
-                _mapboxFeatureCollection = featureCollection
-                _mapboxFeatureCollectionFlow.value = featureCollection
-            }
-        }
+    private val _mapboxColorCollection = MutableStateFlow<List<Color>>(emptyList())
+    val mapboxColorCollection: StateFlow<List<Color>> = _mapboxColorCollection
 
+    fun loadFeatureCollection() {
         viewModelScope.launch {
             getMapboxPointCollection().collect { pointCollection ->
                 _mapboxPointCollection.value = pointCollection
             }
         }
+        viewModelScope.launch {
+            getMapboxColorCollection().collect { colorCollection ->
+                _mapboxColorCollection.value = colorCollection
+            }
+        }
     }
 
-    private fun getMapboxFeatureCollection(): Flow<FeatureCollection> {
+    private fun getMapboxLocationCollection(): Flow<List<MapBoxLocation>> {
         val locations = sensorRepository.locationFlowUi
         val climbrates = sensorRepository.climbrateFlowUi
         val locationList = mutableListOf<Location>()
@@ -60,24 +56,32 @@ class LocationPathViewModel @Inject constructor(
         return combine(locations, climbrates) { l, c ->
             locationList.add(l)
             climbrateList.add(c)
-            mapToFeatureCollection(zipMapBoxLocations(locationList, climbrateList).sortedBy { it.timestamp })
+
+            if (locationList.size > 120) {
+                locationList.removeAt(0)
+            }
+            if (climbrateList.size > 120) {
+                climbrateList.removeAt(0)
+            }
+
+            zipMapBoxLocations(locationList, climbrateList)
+                .sortedBy { it.timestamp }
         }
     }
 
     private fun getMapboxPointCollection(): Flow<List<Point>> {
-        val locations = sensorRepository.locationFlowUi
-        val climbrates = sensorRepository.climbrateFlowUi
-        val locationList = mutableListOf<Location>()
-        val climbrateList = mutableListOf<Climbrate>()
+        return getMapboxLocationCollection().map {
+            it.map { mapBoxLocation ->
+                mapBoxLocation.point
+            }
+        }
+    }
 
-        locationList.clear()
-        climbrateList.clear()
-        return combine(locations, climbrates) { l, c ->
-            locationList.add(l)
-            climbrateList.add(c)
-            zipMapBoxLocations(locationList, climbrateList)
-                .sortedBy { it.timestamp }
-                .map { it.point }
+    private fun getMapboxColorCollection(): Flow<List<Color>> {
+        return getMapboxLocationCollection().map {
+            it.map { mapBoxLocation ->
+                Color(mapBoxLocation.color.toColorInt())
+            }
         }
     }
 }
