d085f8ec9175cecb78ba33d31fea1bf29aa8e7c0
Thomas Fiedler <thomas.fiedler@fn.de>
2025-02-18 19:47:07 +0100

    History tracking feature improved

--- core/model/src/main/java/com/alpsfly/aeroglide/core/model/configuration/ColorMapping.kt
+++ core/model/src/main/java/com/alpsfly/aeroglide/core/model/configuration/ColorMapping.kt
@@ -1,130 +1,94 @@
 package com.alpsfly.aeroglide.core.model.configuration
 
-import android.graphics.Color
-
 class ColorMapping {
 
     private var colorsAscent = mutableListOf(
-        "#96ED89", // 0.0 .. 0.3
-        // https://colordesigner.io/#FFA012-FFB114-FFC412-FFD412-FFE814
-        "#FFA012", // 0.3 ..
-        "#FFB114", // 0.5 ..
-        "#FFC412", // 0.7 ..
-        "#FFD412", // 0.9 ..
-        "#FFE814", // 1.1 ..
-        // https://colordesigner.io/#EF5411-FA5B0F-FF6517-FF6D1F-FF822E
-        "#EF5411", // 1.3
-        "#FA5B0F", // 1.5 ..
-        "#FF6517", // 1.7 ..
-        "#FF6D1F", // 1.9 ..
-        "#FF822E", // 2.1 ..
-        // https://colordesigner.io/#450003-5C0002-94090D-D40D12-FF1D23
-        "#EF5411", // 2.3 ..
-        "#FA5B0F", // 2.5 ..
-        "#FF6517", // 2.7 ..
-        "#FF6D1F", // 2.9 ..
-        "#FF822E"  // 3.1 ..
+        "#96ED89", // 0.0 .. 0.2
+        "#A5F286", // 0.2 .. 0.4
+        "#B4F785", // 0.4 .. 0.6
+        "#C3FC84", // 0.6 .. 0.8
+        "#D2FF83", // 0.8 .. 1.0
+        "#E1FF82", // 1.0 .. 1.2
+        "#FFA012", // 1.2 .. 1.4
+        "#FFB114", // 1.4 .. 1.6
+        "#FFC412", // 1.6 .. 1.8
+        "#FFD412", // 1.8 .. 2.0
+        "#FFE814", // 2.0 .. 2.2
+        "#EF5411", // 2.2 .. 2.4
+        "#FA5B0F", // 2.4 .. 2.6
+        "#FF6517", // 2.6 .. 2.8
+        "#FF6D1F", // 2.8 .. 3.0
+        "#FF822E", // 3.0 ..
+        "#450003"
     )
 
     private var colorsDescent = mutableListOf(
-        "#96ED89", // 0.0 .. 0.5
-        // https://colordesigner.io/#0EEAFF-15A9FA-1B76FF-1C3FFD-2C1DFF
-        "#0EEAFF", // 0.5 ..
-        "#15A9FA", // 0.8 ..
-        "#1B76FF", // 1.1 ..
-        "#1C3FFD", // 1.4 ..
-        "#2C1DFF", // 1.7 ..
-        // https://colordesigner.io/#010340-0E1E8C-0003C7-1510F0-1441F7
-        "#1441F7", // 2.0 ..
-        "#1510F0", // 2.3 ..
-        "#0003C7", // 2.6 ..
-        "#0E1E8C", // 2.9 ..
+        "#96ED89", // 0.0 .. 0.2
+        "#82E4DF", // 0.2 .. 0.4
+        "#6ED9D5", // 0.4 .. 0.6
+        "#5ACECB", // 0.6 .. 0.8
+        "#46C3C1", // 0.8 .. 1.0
+        "#32B8B7", // 1.0 .. 1.2
+        "#1EA7AD", // 1.2 .. 1.4
+        "#0EEAFF", // 1.4 .. 1.6
+        "#15A9FA", // 1.6 .. 1.8
+        "#1B76FF", // 1.8 .. 2.0
+        "#1C3FFD", // 2.0 .. 2.2
+        "#1441F7", // 2.2 .. 2.4
+        "#1510F0", // 2.4 .. 2.6
+        "#0003C7", // 2.6 .. 2.8
+        "#0E1E8C", // 2.8 .. 3.0
         "#010340"  // 3.0 ..
     )
 
-    fun getColorAscent(climbrate: Float) : String {
-        return when ((climbrate * 10f).toInt()) {
-            in 0 .. 3 -> colorsAscent[0]
-            in 3 .. 5 -> colorsAscent[1]
-            in 5 .. 7 -> colorsAscent[2]
-            in 7 .. 9 -> colorsAscent[3]
-            in 9 .. 11 -> colorsAscent[4]
-            in 11 .. 13 -> colorsAscent[5]
-            in 13 .. 15 -> colorsAscent[6]
-            in 15 .. 17 -> colorsAscent[7]
-            in 17 .. 19 -> colorsAscent[8]
-            in 19 .. 21 -> colorsAscent[9]
-            in 21 .. 23 -> colorsAscent[10]
-            in 23 .. 25 -> colorsAscent[11]
-            in 25 .. 27 -> colorsAscent[12]
-            in 27 .. 29 -> colorsAscent[13]
-            in 29 .. 31 -> colorsAscent[14]
-            in 31 .. 33 -> colorsAscent[15]
-            else -> colorsAscent[15]
+    private fun getColorAscent(climbrate: Float): String {
+        return when ((climbrate * 5f).toInt()) {
+            in 0 .. 1 -> colorsAscent[0]
+            in 1 .. 2 -> colorsAscent[1]
+            in 2 .. 3 -> colorsAscent[2]
+            in 3 .. 4 -> colorsAscent[3]
+            in 4 .. 5 -> colorsAscent[4]
+            in 5 .. 6 -> colorsAscent[5]
+            in 6 .. 7 -> colorsAscent[6]
+            in 7 .. 8 -> colorsAscent[7]
+            in 8 .. 9 -> colorsAscent[8]
+            in 9 .. 10 -> colorsAscent[9]
+            in 10 .. 11 -> colorsAscent[10]
+            in 11 .. 12 -> colorsAscent[11]
+            in 12 .. 13 -> colorsAscent[12]
+            in 13 .. 14 -> colorsAscent[13]
+            in 14 .. 15 -> colorsAscent[14]
+            in 15 .. 16 -> colorsAscent[15]
+            else -> colorsAscent[16]
         }
     }
 
-    fun getColorDescent(climbrate: Float) : String {
-        return when ((climbrate * -10f).toInt()) {
-            in 0 .. 5 -> colorsDescent[0]
-            in 5 .. 8 -> colorsDescent[1]
-            in 8 .. 11 -> colorsDescent[2]
-            in 11 .. 14 -> colorsDescent[3]
-            in 14 .. 17 -> colorsDescent[4]
-            in 17 .. 21 -> colorsDescent[5]
-            in 20 .. 23 -> colorsDescent[6]
-            in 23 .. 26 -> colorsDescent[7]
-            in 26 .. 29 -> colorsDescent[8]
-            in 29 .. 31 -> colorsDescent[9]
-            in 31 .. 33 -> colorsDescent[10]
-            else -> colorsDescent[10]
+    private fun getColorDescent(climbrate: Float): String {
+        return when ((climbrate * -5f).toInt()) {
+            in 0 .. 1 -> colorsDescent[0]
+            in 1 .. 2 -> colorsDescent[1]
+            in 2 .. 3 -> colorsDescent[2]
+            in 3 .. 4 -> colorsDescent[3]
+            in 4 .. 5 -> colorsDescent[4]
+            in 5 .. 6 -> colorsDescent[5]
+            in 6 .. 7 -> colorsDescent[6]
+            in 7 .. 8 -> colorsDescent[7]
+            in 8 .. 9 -> colorsDescent[8]
+            in 9 .. 10 -> colorsDescent[9]
+            in 10 .. 11 -> colorsDescent[10]
+            in 11 .. 12 -> colorsDescent[11]
+            in 12 .. 13 -> colorsDescent[12]
+            in 13 .. 14 -> colorsDescent[13]
+            in 14 .. 15 -> colorsDescent[14]
+            else -> colorsDescent[15]
         }
     }
 
-    fun getClimbrateColor(climbrate: Float) : String {
+    fun getClimbrateColor(climbrate: Float): String {
         return if (climbrate > 0) {
             getColorAscent(climbrate)
         } else {
             getColorDescent(climbrate)
-        }
-    }
-
-    fun getGradeColorAscent(grade: Float) : String {
-        return when (grade) {
-            in 0f .. 1f -> colorsAscent[0]
-            in 1f .. 2f -> colorsAscent[1]
-            in 2f .. 3f -> colorsAscent[2]
-            in 3f .. 4f -> colorsAscent[3]
-            in 4f .. 5f -> colorsAscent[4]
-            in 5f .. 6f -> colorsAscent[5]
-            in 6f .. 7f -> colorsAscent[6]
-            in 7f .. 8f -> colorsAscent[7]
-            in 8f .. 9f -> colorsAscent[8]
-            in 9f .. 10f -> colorsAscent[9]
-            in 10f .. 11f -> colorsAscent[10]
-            in 11f .. 12f -> colorsAscent[11]
-            in 12f .. 13f -> colorsAscent[12]
-            in 13f .. 14f -> colorsAscent[13]
-            in 14f .. 15f -> colorsAscent[14]
-            in 15f .. 16f -> colorsAscent[15]
-            else -> colorsAscent[15]
-        }
-    }
-
-    fun getGradeColorDescent(grade: Float) : String {
-        return when (grade.toInt()) {
-            in -0 downTo -1 -> colorsDescent[0]
-            in -1 downTo -2 -> colorsDescent[1]
-            in -2 downTo -3 -> colorsDescent[2]
-            in -3 downTo -4 -> colorsDescent[3]
-            in -4 downTo -5 -> colorsDescent[4]
-            in -5 downTo -6 -> colorsDescent[5]
-            in -6 downTo -8 -> colorsDescent[6]
-            in -8 downTo -10 -> colorsDescent[7]
-            in -10 downTo -12 -> colorsDescent[8]
-            in -12 downTo -14 -> colorsDescent[9]
-            in -14 downTo -16 -> colorsDescent[10]
-            else -> colorsDescent[10]
         }
     }
 }
--- feature/activityhistory/src/main/java/com/alpsfly/aeroglide/feature/activityhistory/MapHistoryScreen.kt
+++ feature/activityhistory/src/main/java/com/alpsfly/aeroglide/feature/activityhistory/MapHistoryScreen.kt
@@ -3,23 +3,23 @@ package com.alpsfly.aeroglide.feature.activityhistory
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.core.graphics.toColorInt
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import com.mapbox.geojson.Feature
-import com.mapbox.geojson.LineString
 import com.mapbox.geojson.Point
-import com.mapbox.maps.Style
 import com.mapbox.maps.extension.compose.MapEffect
 import com.mapbox.maps.extension.compose.MapboxMap
 import com.mapbox.maps.extension.compose.animation.viewport.rememberMapViewportState
-import com.mapbox.maps.extension.style.layers.generated.lineLayer
-import com.mapbox.maps.extension.style.layers.properties.generated.LineCap
-import com.mapbox.maps.extension.style.layers.properties.generated.LineJoin
-import com.mapbox.maps.extension.style.sources.generated.GeoJsonSource
-import com.mapbox.maps.extension.style.sources.generated.geoJsonSource
-import com.mapbox.maps.extension.style.sources.getSourceAs
-import com.mapbox.maps.extension.style.style
+import com.mapbox.maps.extension.compose.annotation.generated.PolylineAnnotation
+import com.mapbox.maps.extension.compose.annotation.generated.PolylineAnnotationGroup
+import com.mapbox.maps.plugin.PuckBearing
+import com.mapbox.maps.plugin.annotation.generated.PolylineAnnotationOptions
+import com.mapbox.maps.plugin.locationcomponent.createDefault2DPuck
+import com.mapbox.maps.plugin.locationcomponent.location
 
 private const val TRACK_SOURCE_ID = "track-source-id"
 private const val TRACK_LAYER_ID = "track-layer-id"
@@ -33,43 +33,18 @@ fun MapHistoryScreen(
     activityId: Long,
     viewModel: MapHistoryViewModel = hiltViewModel(),
 ) {
+    val locations by viewModel.mapboxLocationCollection.collectAsState()
+
     LaunchedEffect(key1 = activityId) {
         viewModel.loadFeatureCollection(activityId)
     }
 
     val mapViewportState = rememberMapViewportState {
         setCameraOptions {
-            center(Point.fromLngLat(9.05419, 48.40567))
+            center(Point.fromLngLat(9.126798, 48.370779))
             zoom(10.0)
             pitch(0.0)
             bearing(0.0)
-        }
-    }
-
-    val mapStyleContract = style(style = Style.OUTDOORS) {
-        +geoJsonSource(id = TRACK_SOURCE_ID) {
-            // url("asset://empty-track.geojson")
-        }
-        +geoJsonSource(id = ROUTE_SOURCE_ID) {
-            // url("asset://empty-track.geojson")
-        }
-        +lineLayer(layerId = TRACK_LAYER_ID, sourceId = TRACK_SOURCE_ID) {
-            lineCap(LineCap.ROUND)
-            lineJoin(LineJoin.ROUND)
-            lineOpacity(0.7)
-            lineWidth(8.0)
-            lineColor(
-                com.mapbox.maps.extension.style.expressions.dsl.generated.get {
-                    literal("color")
-                }
-            )
-        }
-        +lineLayer(layerId = ROUTE_LAYER_ID, sourceId = ROUTE_SOURCE_ID) {
-            lineCap(LineCap.ROUND)
-            lineJoin(LineJoin.ROUND)
-            lineOpacity(0.4)
-            lineWidth(8.0)
-            lineColor("#0d47c1")
         }
     }
 
@@ -77,11 +52,13 @@ fun MapHistoryScreen(
         Modifier.fillMaxSize(),
         mapViewportState = mapViewportState,
     ) {
-        MapEffect(Unit) { mapView ->
-            mapView.mapboxMap.loadStyle(mapStyleContract) { style ->
-                val source = style.getSourceAs<GeoJsonSource>(TRACK_SOURCE_ID)
-                source?.featureCollection(viewModel.mapboxFeatureCollection)
+        PolylineAnnotationGroup(
+            annotations = locations.zipWithNext().map { (start, end) ->
+                PolylineAnnotationOptions()
+                    .withPoints(listOf(start.point, end.point))
+                    .withLineColor(start.color.toColorInt())
+                    .withLineWidth(5.0)
             }
-        }
+        )
     }
 }
