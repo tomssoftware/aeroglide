c03d1547b597c5c785e70144c1c6f4c55fcab48f
Thomas Fiedler <thomas.fiedler@alpsfly.com>
2025-10-21 19:42:44 +0200

    Firebase AppCheck activated

--- app/src/main/java/com/alpsfly/aeroglide/AeroGlideApplication.kt
+++ app/src/main/java/com/alpsfly/aeroglide/AeroGlideApplication.kt
@@ -1,12 +1,19 @@
 package com.alpsfly.aeroglide
 
-import android.util.Log
 import android.app.Application
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import android.util.Log
+import com.google.firebase.Firebase
 import com.google.firebase.FirebaseApp
+import com.google.firebase.appcheck.FirebaseAppCheck
+import com.google.firebase.appcheck.debug.DebugAppCheckProviderFactory
+import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory
+import com.google.firebase.auth.auth
 import com.google.firebase.crashlytics.FirebaseCrashlytics
+import com.google.firebase.firestore.firestore
+import com.google.firebase.firestore.firestoreSettings
 import dagger.hilt.android.HiltAndroidApp
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -15,7 +22,7 @@ import timber.log.Timber
 import javax.inject.Inject
 
 @HiltAndroidApp
-class AeroGlideApplication @Inject constructor (/*private val database: AeroGlideDatabase*/) : Application() {
+class AeroGlideApplication @Inject constructor(/*private val database: AeroGlideDatabase*/) : Application() {
     inner class CrashlyticsTree : Timber.Tree() {
         override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
             when (priority) {
@@ -26,9 +33,11 @@ class AeroGlideApplication @Inject constructor (/*private val database: AeroGlid
                         FirebaseCrashlytics.getInstance().recordException(t)
                     }
                 }
+
                 Log.INFO -> {
                     FirebaseCrashlytics.getInstance().log(message)
                 }
+
                 Log.ASSERT, Log.VERBOSE, Log.DEBUG -> {
                     return
                 }
@@ -37,7 +46,7 @@ class AeroGlideApplication @Inject constructor (/*private val database: AeroGlid
     }
 
 
-   // private val localStorage by lazy { database }
+    // private val localStorage by lazy { database }
 
 //    private val mapStorage: com.alpsfly.aeroglide.core.mapbox.data.MapStorage
 //        get() = com.alpsfly.aeroglide.core.mapbox.data.MapStorage.getInstance()
@@ -57,32 +66,27 @@ class AeroGlideApplication @Inject constructor (/*private val database: AeroGlid
 //    val repository: AeroGlideRepository
 //        get() = AeroGlideRepository.getInstance(localDataSource, webDataSource)
 
-    init {
-
-    }
-
     override fun onCreate() {
         super.onCreate()
         FirebaseApp.initializeApp(this)
-//        val firestore = Firebase.firestore
-//        val auth = Firebase.auth
+        val firestore = Firebase.firestore
+        val auth = Firebase.auth
 
-//        if (BuildConfig.DEBUG) {
-//            val firebaseAppCheck = FirebaseAppCheck.getInstance()
-//            firebaseAppCheck.installAppCheckProviderFactory(DebugAppCheckProviderFactory.getInstance())
-//            firestore.useEmulator(BuildConfig.FIREBASE_EMULATOR_HOST_ADDRESS, BuildConfig.FIREBASE_EMULATOR_PORT_FIRESTORE)
-//            firestore.firestoreSettings = firestoreSettings {
-//                isPersistenceEnabled = false
-//            }
-//            auth.useEmulator(BuildConfig.FIREBASE_EMULATOR_HOST_ADDRESS, BuildConfig.FIREBASE_EMULATOR_PORT_AUTH)
-//            Timber.tag("APP")
-//                .i("Use firebase emulator! Host: ${BuildConfig.FIREBASE_EMULATOR_HOST_ADDRESS}")
-//        } else {
-//            val firebaseAppCheck = FirebaseAppCheck.getInstance()
-//            firebaseAppCheck.installAppCheckProviderFactory(PlayIntegrityAppCheckProviderFactory.getInstance())
-//            Timber.tag("APP")
-//                .i("Use firebase functions! Url: ${BuildConfig.FIREBASE_FUNCTIONS_URL}")
-//        }
+        if (BuildConfig.DEBUG) {
+            val firebaseAppCheck = FirebaseAppCheck.getInstance()
+            firebaseAppCheck.installAppCheckProviderFactory(DebugAppCheckProviderFactory.getInstance())
+            firestore.useEmulator(
+                BuildConfig.FIREBASE_EMULATOR_HOST_ADDRESS,
+                BuildConfig.FIREBASE_EMULATOR_PORT_FIRESTORE
+            )
+            firestore.firestoreSettings = firestoreSettings {
+                isPersistenceEnabled = false
+            }
+            auth.useEmulator(BuildConfig.FIREBASE_EMULATOR_HOST_ADDRESS, BuildConfig.FIREBASE_EMULATOR_PORT_AUTH)
+        } else {
+            val firebaseAppCheck = FirebaseAppCheck.getInstance()
+            firebaseAppCheck.installAppCheckProviderFactory(PlayIntegrityAppCheckProviderFactory.getInstance())
+        }
 
         val channel = NotificationChannel(
             "location",
