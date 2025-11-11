b4fd54f8875967f6285a1b2e049761ecccc4fbf0
Thomas Fiedler <thomas.fiedler@alpsfly.com>
2025-11-11 19:18:30 +0100

    Domain layer architecture added

--- app/src/main/java/com/alpsfly/aeroglide/AeroGlideViewModel.kt
+++ app/src/main/java/com/alpsfly/aeroglide/AeroGlideViewModel.kt
@@ -2,18 +2,14 @@ package com.alpsfly.aeroglide
 
 import androidx.lifecycle.ViewModel
 import com.alpsfly.aeroglide.core.domain.usecase.FlightSessionCoordinatorUseCase
-import com.alpsfly.aeroglide.core.domain.usecase.state.AppStateManager
 import dagger.hilt.android.lifecycle.HiltViewModel
 import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class AeroGlideViewModel @Inject constructor(
-    private val appStateManager: AppStateManager,
     private val flightCoordinator: FlightSessionCoordinatorUseCase
 ) : ViewModel() {
-
-    val appState = appStateManager.appState
 
     fun onToggleRecording() = flightCoordinator.onToggleRecording()
     fun onReCalibrate() = flightCoordinator.startCalibration()
@@ -21,11 +17,6 @@ class AeroGlideViewModel @Inject constructor(
     fun startCalibration() {
         // The ViewModel just tells the coordinator its intent.
         flightCoordinator.startCalibration()
-    }
-
-    fun doEnableAutoStart(enable: Boolean) {
-        // This is a direct state change, so calling AppRepository is fine.
-        appStateManager.onAutoStartEnabled(enable)
     }
 
     override fun onCleared() {
