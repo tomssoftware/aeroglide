40c2c226d51b0cb215edd7a7949c500ee3657fb9
Thomas Fiedler <thomas.fiedler@alpsfly.com>
2025-11-17 21:18:45 +0100

    Billing client added

--- core/ui/src/main/java/com/alpsfly/aeroglide/core/Screen.kt
+++ core/ui/src/main/java/com/alpsfly/aeroglide/core/Screen.kt
@@ -6,4 +6,5 @@ sealed class Screen(val route: String) {
     data object DiagnosisScreen : Screen(route = "DiagnosisScreen")
     data object VariometerScreen : Screen(route = "VariometerScreen")
     data object DataExportScreen : Screen(route = "DataExportScreen")
+    data object PremiumsScreen : Screen(route = "PremiumsScreen")
 }
